7dcc676f18538f0c3376f09f8ded1f79f3e4fe74
erzhigitwx <110293971+erzhigitwx@users.noreply.github.com>
2023-03-24 17:01:22 +0600

    add commit #5

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { store } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={"/react-sneakers"}>
     <ErrorBoundary>
       <Provider store={store}>
         <App />
